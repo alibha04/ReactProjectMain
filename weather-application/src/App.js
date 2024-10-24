96f8908e26feaba3e4fef2e31acf47b1a1c94602
alibha04 <alibha.pra@gmail.com>
2024-10-24 16:36:36 +0000

    weather app completed

--- weather-application/src/App.js
+++ weather-application/src/App.js
@@ -75,7 +75,7 @@ function GfGWeatherApp() {
 	return (
 		<div className="App">
 			<h1 className="app-name">
-				GeeksforGeeks Weather App
+			Weather Application
 			</h1>
 			<div className="search-bar">
 				<input
